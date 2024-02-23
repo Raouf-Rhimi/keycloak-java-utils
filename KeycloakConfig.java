5d13bc00909432503b1532a40ceaec9c117b0975
Raouf-Rhimi <118525888+Raouf-Rhimi@users.noreply.github.com>
2024-02-23 17:55:43 +0100

    Create KeycloakConfig.java

---
+++ KeycloakConfig.java
@@ -0,0 +1,9 @@
+public class KeycloakConfig {
+
+    public static final String KEYCLOAK_URL = "htt://{{keycloak_host}}:{{keycloak_port}}";
+    public static final String USERNAME = "KEYCLOAK_ADMIN_USERNAME";
+    public static final String PASSWORD = "KEYCLOAK_ADMIN_PASSWORD";
+    public static final String ADMIN_CLIENT = "admin-cli";
+
+  
+}
